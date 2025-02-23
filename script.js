da4ec3b8d12679923944b6af6f031ffbf0b99f2c
Yashasvi74 <yashasvi07k@gmail.com>
2025-02-23 17:57:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Change navbar background on scroll
+window.addEventListener("scroll", function () {
+    let navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+}
